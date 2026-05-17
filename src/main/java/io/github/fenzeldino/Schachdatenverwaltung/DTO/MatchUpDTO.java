204fe593df7bddee702b8a013ce76e2814400a7c
Daniel Menzel <menzeldaniel2@gmail.com>
2026-05-17 11:33:34 +0200

    SpielerDTO und MatchUpDTO hinzugefügt

---
+++ src/main/java/io/github/fenzeldino/Schachdatenverwaltung/DTO/MatchUpDTO.java
@@ -0,0 +1,4 @@
+package io.github.fenzeldino.Schachdatenverwaltung.DTO;
+
+public record MatchUpDTO(Integer MatchUpId,String SpielerNameEins,String SpielerNameZwei) {
+}
---
+++ src/main/java/io/github/fenzeldino/Schachdatenverwaltung/DTO/SpielerDTO.java
@@ -0,0 +1,5 @@
+package io.github.fenzeldino.Schachdatenverwaltung.DTO;
+
+public record SpielerDTO(Integer spielerId,String Name,Double rating) {
+
+}
