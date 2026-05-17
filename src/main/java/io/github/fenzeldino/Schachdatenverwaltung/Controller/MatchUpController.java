7382bc4c79bf6bd252d4b159fe75efda57d11c25
Daniel Menzel <menzeldaniel2@gmail.com>
2026-05-17 17:58:04 +0200

    MatchUpService create MatchUp methode

--- src/main/java/io/github/fenzeldino/Schachdatenverwaltung/Controller/MatchUpController.java
+++ src/main/java/io/github/fenzeldino/Schachdatenverwaltung/Controller/MatchUpController.java
@@ -1,6 +1,12 @@
 package io.github.fenzeldino.Schachdatenverwaltung.Controller;
 
+import io.github.fenzeldino.Schachdatenverwaltung.DTO.MatchUpDTO;
+import io.github.fenzeldino.Schachdatenverwaltung.Service.MatchUpService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 
 public class MatchUpController {
+
+    private final MatchUpService matchUpService;
+
+    public MatchUpController(MatchUpService matchUpService) {
+        this.matchUpService = matchUpService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchUpDTO> create(@RequestBody MatchUpDTO matchUpDTO){
+        MatchUpDTO created = matchUpService.createMatUp(matchUpDTO);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
 
 }
