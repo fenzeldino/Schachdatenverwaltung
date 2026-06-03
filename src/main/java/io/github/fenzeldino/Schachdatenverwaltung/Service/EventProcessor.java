9844b0e5fee0c8add108bd81b2007c329f751efd
Daniel Menzel <daniel.menzel2@stud.htw-dresden.de>
2026-06-03 14:16:44 +0200

    EventProcessor und DomainEvent implementiert

--- src/main/java/io/github/fenzeldino/Schachdatenverwaltung/Service/EventProcessor.java
+++ src/main/java/io/github/fenzeldino/Schachdatenverwaltung/Service/EventProcessor.java
@@ -1,4 +1,21 @@
 package io.github.fenzeldino.Schachdatenverwaltung.Service;
 
+import io.github.fenzeldino.Schachdatenverwaltung.Event.DomainEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(EventProcessor.class);
+
+    List logList = new ArrayList<DomainEvent>();
+
+    public void process(DomainEvent domainEvent){
+        log.info("process "+domainEvent);
+        domainEvent.process();
+        logList.add(domainEvent);
+    }
 }
