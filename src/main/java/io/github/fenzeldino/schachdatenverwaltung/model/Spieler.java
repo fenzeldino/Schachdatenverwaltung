ba45700093d57d3c2cca2fe73e7ae37240d84c5f
Daniel Menzel <menzeldaniel2@gmail.com>
2026-08-02 22:04:54 +0200

    SpielerId auf GenerationType.IDENTITY geändert

--- src/main/java/io/github/fenzeldino/schachdatenverwaltung/model/Spieler.java
+++ src/main/java/io/github/fenzeldino/schachdatenverwaltung/model/Spieler.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Spieler {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int spielerId;
     @Column(name="SpielerName")
     private String name;
